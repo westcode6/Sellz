af46dec0e0942ddde7b73899f95964a025264069
westcode6 <westcode6@gmail.com>
2024-01-06 04:39:28 +0100

    oya go

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,7 @@ export const ServiceCard = ({ index, title, content, icon }) => {
           scale: 1,
           speed: 450,
         }}
-        className="w-full mt-10 border bg-white shadow-inner hover:shadow-2xl transform transition  duration-300  rounded-2xl flex flex-col  items-center text-center justify-evenly gap-2"
+        className="w-full mt-10 border bg-white shadow-inner hover:shadow-sm transform transition  duration-300  rounded-2xl flex flex-col  items-center text-center justify-evenly gap-2"
       >
         <div className="w-28 py-4">
           <img src={icon} className="w-full object-contain obeject-center" alt="" />
@@ -78,7 +78,7 @@ const About = ({index}) => {
 
         <div className="w-full  py-12 sm:py-20">
                
-          <div className="w-full flex flex-col lg:flex-row  items-center justify-center px-5 gap-10 pb-10">
+          <div className="w-full flex flex-col lg:flex-row  items-center justify-center px-5 gap-10 pb-20">
             {services.map((service, index) => (
               <ServiceCard key={service.title} index={index} {...service} />
             ))}
